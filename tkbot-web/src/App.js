792bf58f25a100e4a7bdf780c09ff6ef2016e1ba
JacobLayton <jlayjlay3@gmail.com>
2019-08-28 16:37:50 -0700

    Created routes for both views

--- tkbot-web/src/App.js
+++ tkbot-web/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import { withRouter } from "react-router";
+
 import "./App.css";
+
 import Landing from "./components/Landing/Landing";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Landing />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/dashboard" component={Dashboard} />
       </div>
     );
   }
