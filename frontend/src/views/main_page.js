46c09a7d4977854eaaaa1da35b0396380cabb38c
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-08-29 18:19:30 -0500

    added temp signout button to clear localstorage. Set up sign in flow, once user is redirected back to webapp their token is saved to local storage

--- frontend/src/views/main_page.js
+++ frontend/src/views/main_page.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Slb from '../components/slack_login_button.js';
 
-const MainPage = () => {
+const MainPage = props => {
+  const signOut = () => {
+    localStorage.clear();
+    props.history.push('/');
+  }
   if(!localStorage.getItem('id')){
     return <Slb/>
   } else {
     return (
-      <div>Hello world</div>
+      <>
+        <button onClick={() => signOut()}>Sign Out</button>
+        <div>Hello world</div>
+      </>
     );
   }
 };
--- frontend/src/views/slack_login.js
+++ frontend/src/views/slack_login.js
@@ -3,7 +3,6 @@ const SlackLogin = props => {
   if(props.location.search && !localStorage.getItem('id')){
     const str = props.location.search.substring(1);
     localStorage.setItem('id', str);
-    console.log(props);
   }
   props.history.push('/');
 
