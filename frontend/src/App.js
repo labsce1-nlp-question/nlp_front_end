f125f922a8d54969dbd4e02a8ee9b40d21b31b19
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-09-09 18:20:33 -0500

    fixed issue where user history wasn't being sent along with the question results

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import MainPage from './views/MainPage.js';
 import SlackLogin from './views/SlackLogin.js';
+import NotesView from './views/NotesView.js';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Route exact path = '/' component = {MainPage}/>
       <Route path = '/slack-login' component = {SlackLogin}/>
+      <Route path = '/history/:id' component = {NotesView}/>
     </div>
   );
 }
---
+++ frontend/src/views/NotesView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Notesview extends React.Component {
+  state = {
+
+  };
+  render(){
+    return(
+      <div>Take notes here</div>
+    );
+  };
+};
+
+export default Notesview;
