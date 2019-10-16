d9eab52e85fe90b7c5eb6894343a784048b220e6
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-10-16 17:10:19 -0500

    Updated Login page CSS to be more inline with the CSS for the learn lambda school login page

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import SlackLoginButton from './SlackLoginButton.js';
+import Lambda_Logo from '../imgs/Lambda_Logo.png';
 
 const LoginPage = () => {
   return (
     <div className="login-page-wrapper">
-      <div className="login-text">
+      <header>
+        <img 
+          className="logo" 
+          src={Lambda_Logo}
+          alt="logo"
+        />
         <h1>Lambda Training Kit Bot</h1>
-        <h2>Sign in with your Lambda School Slack account</h2>
+      </header>
+      <div className="login-text-wrapper">
+        <div className="login-text">
+          <h1>Sign in</h1>
+          {/* <p>You need to sign in with your Lambda School Slack account to take advantage of this web app</p> */}
+          <SlackLoginButton />
+        </div>
       </div>
-      <SlackLoginButton />
+      <footer>
+        <img 
+          className="logo" 
+          src="https://assets-global.website-files.com/5ca6aa5b04fdce3dfc90bd80/5cb54224f6b7462aac67e7a5_lambda-newlogo-white.png" 
+          alt="logo"
+        />
+      </footer>
     </div>
   );
 };
