a284db310070f69390fbcd652c31dfd8aff0bc69
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-10-31 12:46:26 -0500

    Fixed CSS bug with the new expand to view results feature. Still a work in progress

--- frontend/src/components/SearchHistory.js
+++ frontend/src/components/SearchHistory.js
@@ -5,24 +5,34 @@ import QuestionResult from './QuestionResult';
 
 const SearchHistory = ({ history }) => {
   const [ showResults, setShowResults ] = useState(false);
+  const [ isExpanded, setisExpanded ] = useState('not-expanded');
 
   const toggleExpand = e => {
-    
     //set the state to the opposite of what it currently is
     setShowResults(!showResults);
     //if showResults is true add expand class other wise remove it
-    return !showResults ? e.target.parentElement.classList.add('expand') : e.target.parentElement.classList.remove('expand');
+    return !showResults ? setisExpanded('expanded') : setisExpanded('not-expanded');
   }
 
   return(
-    <tr className="table-data" onClick={e => toggleExpand(e)}>
+    <tr className={`table-data ${isExpanded}`} onClick={e => toggleExpand(e)}>
       <td className="table-question-data">
         {history.question}
-        {showResults ? history.bot_response.match.map(result => {
+        {/* {history.bot_response.match.map(result => {
+          return (
+            <div className="table-results">
+              <a href={result.URL}>
+                <h2>{result.name}</h2>
+                <p>{result.URL}</p>
+              </a>
+            </div>
+          )
+        })} */}
+        {history.bot_response.match.map(result => {
           return(
             <QuestionResult result={result} key={result.id} showDesc={false}/>
           );
-        }) : null}
+        })}
       </td>
       {/* Add ability to click the question and have the bot response results listed below 
           expand the row to fit the results, add UI for expanding ability, Create modal for 
